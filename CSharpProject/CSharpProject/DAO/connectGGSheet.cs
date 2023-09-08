030fbcd80ccaad057ce0d7e4f10f8d24a00760b2
ThaoMinh2907 <nguyenminhthao12b1@gmail.com>
2023-09-08 20:33:37 +0700

    Update API

--- CSharpProject/CSharpProject/DAO/connectGGSheet.cs
+++ CSharpProject/CSharpProject/DAO/connectGGSheet.cs
@@ -12,7 +12,7 @@ namespace CSharpProject.DAO
         public static SheetsService GetSheetsService()
         {
             // Đường dẫn đến tệp JSON chứa thông tin xác thực OAuth 2.0
-            string credentialFilePath = "/credentials.json";
+            string credentialFilePath = "/Users/minhthao/CSharp/CSharpProject/CSharpProject/CSharpProject/DAO/credentials.json";
 
             // Xác thực với tệp JSON
             GoogleCredential credential;
